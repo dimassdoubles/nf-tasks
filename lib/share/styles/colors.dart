55c4a4ac1ccf5757ce5d75e2286aaffad0d30b2f
dimassdoubles <codedimas@gmail.com>
2022-10-13 20:17:42 +0700

    initial commit

---
+++ lib/share/styles/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color black = const Color.fromARGB(255, 25, 24, 42);
+Color blue = const Color.fromARGB(255, 59, 105, 214);
+Color pink = const Color.fromARGB(255, 218, 15, 183);
+Color yellow = const Color.fromARGB(255, 239, 232, 71);
+Color gray = const Color.fromARGB(255, 232, 229, 241);
+Color tosca = const Color.fromARGB(255, 31, 211, 247);
