29babc35fa0ddb901f641dfd2c88312164bc3dad
dimassdoubles <codedimas@gmail.com>
2022-10-15 20:54:37 +0700

    add route
    add route to navigate to home page

--- lib/share/routes.dart
+++ lib/share/routes.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import '../presentation/pages/home_page.dart';
 import '../presentation/pages/splash_page.dart';
 import '../presentation/pages/login_page.dart';
 
 const String splashPage = "splash-page";
 const String loginPage = "login-page";
+const String homePage = "home-page";
 
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
+    case homePage:
+      return MaterialPageRoute(
+        builder: (context) => HomePage(),
+      );
     case splashPage:
       return MaterialPageRoute(
         builder: (context) => const SplashPage(),
       );
     case loginPage:
       return MaterialPageRoute(
-        builder: (context) => const LoginPage(),
+        builder: (context) => LoginPage(),
       );
     default:
       throw ("This route name doesn't exist");
